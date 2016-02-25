f3e7ebbc21b537e31cc70ceea4dc6387acef70f5
Michael Sturrus <msturrus@gmail.com>
2016-02-25 11:17:45 -0600

    gitgong went off again it keeps happening

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // we want to wait for all the things
 // on the page to load before doing anything!
 
-window.onload = function(){
+window.onload = function(event){
 // anything called or done inside of here will only occur
 // after the page has loaded every image, file, style, etc.
 console.log('hey friends!  we made it :)');
